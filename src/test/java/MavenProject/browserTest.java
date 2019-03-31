ef09882a8e1c3048007a0348578eb44d492306e2
kunal8147 <kunal.gautam30@gmail.com>
2019-04-01 00:42:05 +0530

    first code commit

---
+++ src/test/java/MavenProject/browserTest.java
@@ -0,0 +1,27 @@
+package MavenProject;
+
+import org.testng.annotations.Test;
+
+public class browserTest {
+	
+	
+	@Test
+	public void BrowserTest() {
+		
+		System.out.println("BrowserTest");
+		
+		
+	}
+	
+	@Test
+	public void BrowserTest1() {
+		
+		System.out.println("BrowserTest1");
+	
+		
+		
+		
+	}
+
+	
+}
---
+++ src/test/java/MavenProject/mobileTest.java
@@ -0,0 +1,26 @@
+package MavenProject;
+
+import org.testng.annotations.Test;
+
+public class mobileTest {
+	
+	@Test
+	public void MobileTest() {
+		
+		System.out.println("MobileTest");
+		
+		
+		
+	}
+	
+	@Test
+	public void obileTest1() {
+		
+		System.out.println("MobileTest1");
+		
+		
+		
+	}
+
+
+}
